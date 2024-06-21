35c2baefbbb3a9ee4fe6e2f18dfc1f987f692afa
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-06-21 20:00:37 +0300

    Added the .env file

--- src/Atoms/UserCard/index.js
+++ src/Atoms/UserCard/index.js
@@ -1,19 +1,5 @@
 
-// const UserCard = ({image,fullName}) =>{
-
-//     return (
-//         <div>
-//             <img src={image} alt={`${fullName} profile`}/>
-//             <h3>{fullName}</h3>
-//         </div>
-//     )
-// }
-
-// export default UserCard;
-
-
-
-function UserCard({image , fullName,age,gender,email}){
+const UserCard = ({image , fullName, age, gender, email, nationality}) => {
     return(
         <div>
             <img src={image} alt={`${fullName} profile`}/>
@@ -21,7 +7,9 @@ function UserCard({image , fullName,age,gender,email}){
             <p>{age}</p>
             <p>{gender}</p>
             <p>{email}</p>
+            <p>{nationality}</p>
         </div>
     )
 }
+
 export default UserCard;
--- src/Login/index.js
+++ src/Login/index.js
@@ -2,21 +2,18 @@ import "./index.css";
 import { useState } from "react";
 import { login } from "./utils";
 import { Link, useNavigate} from 'react-router-dom';
-
-
     
-
 
 const Login = () =>{
+
     const navigate = useNavigate();
-    
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = async(event) =>{
         event.preventDefault();
         const results = await login({username, password});
-        navigate('./users')
+        navigate('./Users')
 
         console.log({results});
     }
@@ -24,11 +21,11 @@ const Login = () =>{
     return(
         <form onSubmit={handleSubmit}>
             <h2>Login</h2>
-            <input  placeholder="Enter username" type="text" onChange={(event)=>setUsername(event.target.value)}/>
+            <input required placeholder="Enter username" type="text" onChange={(event)=>setUsername(event.target.value)}/>
             <br/>
-            <input  placeholder="Enter password" type="password" onChange={(event)=>setPassword(event.target.value)}/>
+            <input required placeholder="Enter password" type="password" onChange={(event)=>setPassword(event.target.value)}/>
             <br/>
-           <Link to="/users"> <button type="submit">Login</button> </Link>
+           <Link to="/Users"> <button type="submit">Login</button> </Link>
 
         </form>
     )
